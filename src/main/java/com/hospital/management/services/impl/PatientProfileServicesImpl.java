c4d978171ac01e782f4d73ee07962b0cc3a2266b
PANDITHURAI-M <pandithurai.marimuthu1@gmail.com>
2025-06-19 20:29:46 +0530

    thursday update

--- src/main/java/com/hospital/management/services/impl/PatientProfileServicesImpl.java
+++ src/main/java/com/hospital/management/services/impl/PatientProfileServicesImpl.java
@@ -19,7 +19,7 @@ public class PatientProfileServicesImpl implements ProfileService{
 		
 		String driverURL = "com.mysql.cj.jdbc.Driver";
 		String connectionURL="jdbc:mysql://localhost:3306/Hospital";
-		String dbUser="root";
+		String user="root";
 		String dbPassWord="password";
 		String insertQuery="insert into patient(name,age,address,mobile,gender) values(?,?,?,?,?)";
 	
@@ -33,7 +33,7 @@ public class PatientProfileServicesImpl implements ProfileService{
 		
 		try {
 			// Getting the connection via driver
-			Connection connection = DriverManager.getConnection(connectionURL,dbUser,dbPassWord);
+			Connection connection = DriverManager.getConnection(connectionURL,user,dbPassWord);
 			
 			PreparedStatement preparedStatement =connection.prepareStatement(insertQuery);
 			
