8d5c2092b18cb361cb428b8371e93440289cea54
PANDITHURAI-M <pandithurai.marimuthu1@gmail.com>
2025-08-14 21:03:20 +0530

    Thursday

--- src/main/java/com/hospital/management/controller/DoctorProfileUpdateController.java
+++ src/main/java/com/hospital/management/controller/DoctorProfileUpdateController.java
@@ -1,8 +1,8 @@
 package com.hospital.management.controller;
 
-import java.io.IOException;
 
-import javax.servlet.ServletException;
+
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
